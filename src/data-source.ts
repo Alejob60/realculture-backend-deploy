63f7a19ff8d0281a73133d27d2307b92e9d531b2
LabsCode <labscode2026@outlook.com>
2025-06-22 19:34:16 -0500

    Fix Deploy Azure

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,21 @@
-// src/data-source.ts
 import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
 import { UserEntity } from './domain/entities/user.entity';
 import { Content } from './domain/entities/content.entity';
 
+// Carga variables desde .env o .env.production
+config();
+
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost', // o 'db' si estás usando docker-compose y el servicio se llama así
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'realculture',
-  synchronize: false,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: false, // ⚠️ No uses true en producción
   logging: false,
+  ssl: process.env.SSL === 'true' ? { rejectUnauthorized: false } : false,
   entities: [UserEntity, Content],
   migrations: ['src/migrations/*.ts'],
 });
